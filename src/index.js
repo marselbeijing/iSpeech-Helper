45781bd8078a6c59efe1c458bc8b285e6758b359
Marsel Sharipov <sharipovmarsel@gmail.com>
2025-06-11 20:54:57 +0500

    fix(analytics): Обновлен URL для Vercel и убраны скрипты gh-pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import telegramAnalytics from '@telegram-apps/analytics';
+import WebApp from '@twa-dev/sdk';
 
-// Initialize Telegram Analytics
-telegramAnalytics.init({
+// Initialize Telegram Analytics ONLY if running inside Telegram
+if (WebApp.platform !== 'unknown' && process.env.NODE_ENV === 'production') {
+  telegramAnalytics.init({
     token: 'b3a58e2a-d242-4368-a0d7-5421293a525f', // SDK Auth token
     appName: 'iSpeech Helper', // The analytics identifier
-});
+  });
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
