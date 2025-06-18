8846b5f183e3f539a6b6d97bd3dd822f66931e9a
Marsel Sharipov <sharipovmarsel@gmail.com>
2025-06-18 18:47:26 +0500

    Добавлена тестовая версия Analytics SDK для диагностики - Создан analytics-test.js с детальным логированием - Переключен App.js на тестовую версию - Добавлен глобальный метод window.testTelegramAnalytics() - Можно проверить работу SDK через консоль браузера

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import WebApp from '@twa-dev/sdk';
 import './i18n';
 import { useTranslation } from 'react-i18next';
 import { initTelegramWebApp } from './services/telegram';
-import analyticsService from './services/analytics-simple';
+import analyticsService from './services/analytics-test';
 
 // Components
 import Root from './components/Root';
