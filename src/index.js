79c8dec977d8ac611678bf7aafb77b38bbf69f0b
Marsel Sharipov <sharipovmarsel@gmail.com>
2025-06-20 22:05:08 +0500

    fix: обёртка приложения в I18nextProvider для корректной работы локализации и useTranslation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
 
 // Analytics уже инициализируется в сервисе
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
